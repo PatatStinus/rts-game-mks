939b972235be08250e89a68135cf4ec7f633121a
PatatStinus <71143317+PatatStinus@users.noreply.github.com>
2021-03-05 15:25:37 +0100

    Added working AIMovement

--- RTSGame/RTSGame/Assets/Scripts/AIMovement.cs
+++ RTSGame/RTSGame/Assets/Scripts/AIMovement.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIMovement : MonoBehaviour
 {
-    public Camera cam;
+    [SerializeField] private Camera cam;
+    private NavMeshAgent simpleMovement;
+    private GameObject indicator;
+    private bool playerSelected;
 
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Player" && playerSelected == false)
+                {
+                    playerSelected = true;
+                    simpleMovement = hit.transform.GetComponent<NavMeshAgent>();
+                    //indicator.SetActive(true);
+                    return;
+                }
+
+                if(playerSelected == true)
+                {
+                    playerSelected = false;
+                    //indicator.SetActive(false);
+                    simpleMovement.SetDestination(hit.point);
+                }
+            }
         }
     }
 }
