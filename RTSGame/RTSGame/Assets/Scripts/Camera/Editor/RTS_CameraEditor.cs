eafe3acd80edf728d9f2d6e7fd11b543bf8a8617
Kaithlyn <kavandeelen@st.noorderpoort.nl>
2021-03-08 15:09:00 +0100

    Added camera script

---
+++ RTSGame/RTSGame/Assets/Scripts/Camera/Editor/RTS_CameraEditor.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace RTS_Cam
+{
+    [CustomEditor(typeof(RTS_Camera))]
+    public class RTS_CameraEditor : Editor
+    {
+        private RTS_Camera camera { get { return target as RTS_Camera; } }
+
+        private TabsBlock tabs;
+
+        private void OnEnable()
+        {
+            tabs = new TabsBlock(new Dictionary<string, System.Action>() 
+            {
+                {"Movement", MovementTab},
+                {"Height", HeightTab}
+            });
+            tabs.SetCurrentMethod(camera.lastTab);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            //base.OnInspectorGUI();
+            Undo.RecordObject(camera, "RTS_CAmera");
+            tabs.Draw();
+            if (GUI.changed)
+                camera.lastTab = tabs.curMethodIndex;
+            EditorUtility.SetDirty(camera);
+        }
+
+        private void MovementTab()
+        {
+            using (new HorizontalBlock())
+            {
+                GUILayout.Label("Use keyboard input: ", EditorStyles.boldLabel, GUILayout.Width(170f));
+                camera.useKeyboardInput = EditorGUILayout.Toggle( camera.useKeyboardInput);
+            }
+            if(camera.useKeyboardInput)
+            {
+                camera.horizontalAxis = EditorGUILayout.TextField("Horizontal axis name: ", camera.horizontalAxis);
+                camera.verticalAxis = EditorGUILayout.TextField("Vertical axis name: ", camera.verticalAxis);
+                camera.keyboardMovementSpeed = EditorGUILayout.FloatField("Movement speed: ", camera.keyboardMovementSpeed);
+            }
+
+            using (new HorizontalBlock())
+            {
+                GUILayout.Label("Panning with mouse: ", EditorStyles.boldLabel, GUILayout.Width(170f));
+                camera.usePanning = EditorGUILayout.Toggle(camera.usePanning);
+            }
+            if(camera.usePanning)
+            {
+                camera.panningKey = (KeyCode)EditorGUILayout.EnumPopup("Panning when holding: ", camera.panningKey);
+                camera.panningSpeed = EditorGUILayout.FloatField("Panning speed: ", camera.panningSpeed);
+            }
+
+        }
+
+        private void HeightTab()
+        {
+            using (new HorizontalBlock())
+            {
+                GUILayout.Label("Auto height: ", EditorStyles.boldLabel, GUILayout.Width(170f));
+                camera.autoHeight = EditorGUILayout.Toggle(camera.autoHeight);
+            }
+            if (camera.autoHeight)
+            {
+                camera.heightDampening = EditorGUILayout.FloatField("Height dampening: ", camera.heightDampening);
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("groundMask"));
+            }
+            using (new HorizontalBlock())
+            {
+                GUILayout.Label("Scrollwheel zooming: ", EditorStyles.boldLabel, GUILayout.Width(170f));
+                camera.useScrollwheelZooming = EditorGUILayout.Toggle(camera.useScrollwheelZooming);
+            }
+            if (camera.useScrollwheelZooming)
+                camera.scrollWheelZoomingSensitivity = EditorGUILayout.FloatField("Scrollwheel sensitivity: ", camera.scrollWheelZoomingSensitivity);
+
+            if (camera.useScrollwheelZooming)
+            {
+                using (new HorizontalBlock())
+                {
+                    camera.maxHeight = EditorGUILayout.FloatField("Max height: ", camera.maxHeight);
+                    camera.minHeight = EditorGUILayout.FloatField("Min height: ", camera.minHeight);
+                }
+            }  
+        }
+    }
+}
---
+++ RTSGame/RTSGame/Assets/Scripts/Camera/RTS_Camera.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RTS_Cam
+{
+    [RequireComponent(typeof(Camera))]
+    [AddComponentMenu("RTS Camera")]
+    public class RTS_Camera : MonoBehaviour
+    {
+        public int lastTab = 0;
+        public bool movementSettingsFoldout;
+        public bool zoomingSettingsFoldout;
+        public bool heightSettingsFoldout;
+        public bool inputSettingsFoldout;
+        private Transform m_Transform;
+        public bool useFixedUpdate = false;
+        public float keyboardMovementSpeed = 5f;
+        public float panningSpeed = 5f;
+        public bool autoHeight = true;
+        public LayerMask groundMask = -1;
+        public float maxHeight = 5f;
+        public float minHeight = 5f;
+        public float heightDampening = 1f; 
+        public float scrollWheelZoomingSensitivity = 25f;
+        private float zoomPos = 0;
+        public bool useKeyboardInput = true;
+        public string horizontalAxis = "Horizontal";
+        public string verticalAxis = "Vertical";
+        public bool usePanning = true;
+        public KeyCode panningKey = KeyCode.Mouse1;
+        public bool useScrollwheelZooming = true;
+        public string zoomingAxis = "Mouse ScrollWheel";
+
+        private Vector2 KeyboardInput
+        {
+            get { return useKeyboardInput ? new Vector2(Input.GetAxis(horizontalAxis), Input.GetAxis(verticalAxis)) : Vector2.zero; }
+        }
+        private Vector2 MouseInput
+        {
+            get { return Input.mousePosition; }
+        }
+        private float ScrollWheel
+        {
+            get { return Input.GetAxis(zoomingAxis); }
+        }
+        private Vector2 MouseAxis
+        {
+            get { return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")); }
+        }
+        private void Start()
+        {
+            m_Transform = transform;
+        }
+        private void Update()
+        {
+            if (!useFixedUpdate)
+                CameraUpdate();
+        }
+        private void FixedUpdate()
+        {
+            if (useFixedUpdate)
+                CameraUpdate();
+        }
+        private void CameraUpdate()
+        {
+            Move();
+            HeightCalculation();
+        }
+        private void Move()
+        {
+            if (useKeyboardInput)
+            {
+                Vector3 desiredMove = new Vector3(KeyboardInput.x, 0, KeyboardInput.y);
+
+                desiredMove *= keyboardMovementSpeed;
+                desiredMove *= Time.deltaTime;
+                desiredMove = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * desiredMove;
+                desiredMove = m_Transform.InverseTransformDirection(desiredMove);
+
+                m_Transform.Translate(desiredMove, Space.Self);
+            }
+            if(usePanning && Input.GetKey(panningKey) && MouseAxis != Vector2.zero)
+            {
+                Vector3 desiredMove = new Vector3(-MouseAxis.x, 0, -MouseAxis.y);
+
+                desiredMove *= panningSpeed;
+                desiredMove *= Time.deltaTime;
+                desiredMove = Quaternion.Euler(new Vector3(0f, transform.eulerAngles.y, 0f)) * desiredMove;
+                desiredMove = m_Transform.InverseTransformDirection(desiredMove);
+
+                m_Transform.Translate(desiredMove, Space.Self);
+            }
+        }
+        private void HeightCalculation()
+        {
+            float distanceToGround = DistanceToGround();
+            if(useScrollwheelZooming)
+                zoomPos += ScrollWheel * Time.deltaTime * scrollWheelZoomingSensitivity;
+
+            zoomPos = Mathf.Clamp01(zoomPos);
+
+            float targetHeight = Mathf.Lerp(minHeight, maxHeight, zoomPos);
+            float difference = 0; 
+
+            if(distanceToGround != targetHeight)
+                difference = targetHeight - distanceToGround;
+
+            m_Transform.position = Vector3.Lerp(m_Transform.position, 
+                new Vector3(m_Transform.position.x, targetHeight + difference, m_Transform.position.z), Time.deltaTime * heightDampening);
+        }
+        private float DistanceToGround()
+        {
+            Ray ray = new Ray(m_Transform.position, Vector3.down);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, groundMask.value))
+                return (hit.point - m_Transform.position).magnitude;
+
+            return 0f;
+        }
+    }
+}
