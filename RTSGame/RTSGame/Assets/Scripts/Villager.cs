38a16eca664b1a517f008081020a4696e322e558
MarkJ <mjansen36@st.noorderpoort.nl>
2021-03-08 14:10:16 +0100

    TmPro added + physics + villagerspawn

---
+++ RTSGame/RTSGame/Assets/Scripts/Villager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Villager : MonoBehaviour
+{
+    private Transform civSpawnLocation;
+    public GameObject objectToCreate;
+    public GameObject civSpawn;
+    private float cooldownTime = 10f;
+    public TextMeshProUGUI villagerQueue;
+    public Image circleTimer;
+    private int vSpawnCount;
+
+    private void Update()
+    {
+        if (vSpawnCount > 0)
+        {
+            cooldownTime -= 1 * Time.deltaTime;
+            if (cooldownTime <= 0f)
+            {
+                civSpawnLocation = civSpawn.transform;
+                Instantiate(objectToCreate, civSpawnLocation);
+                cooldownTime = 10f;
+                vSpawnCount--;
+            }
+
+            villagerQueue.text = vSpawnCount.ToString("0");
+            circleTimer.fillAmount = cooldownTime/10;
+        }
+    }
+    public void SpawnObject()
+    {
+        vSpawnCount++;
+        if (vSpawnCount > 10)
+            vSpawnCount = 10;
+    }
+}
